8e323bf3d33595dc3beb3e58b7b78a801857fd67
Gustavo Tsuru <g.tsuruendo@gmail.com>
2025-05-09 17:58:50 -0300

    :tada: init: Commit inicial

---
+++ src/main/java/br/fatec/TemosVagas/TemosVagasApplication.java
@@ -0,0 +1,14 @@
+package br.fatec.TemosVagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemosVagasApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TemosVagasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/fatec/TemosVagas/TemosVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fatec.TemosVagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemosVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
