2c3182d403e18caf150eabbb7b3c54eeee7cc4b7
Gustavo Tsuru <g.tsuruendo@gmail.com>
2025-06-04 23:14:04 -0300

    :goal_net: feat: Adicionando tratamento global de exceções

--- src/main/java/br/fatec/TemosVagas/exceptions/GlobalExceptionHandler.java
+++ src/main/java/br/fatec/TemosVagas/exceptions/GlobalExceptionHandler.java
@@ -14,45 +14,61 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorsDTO> handleValidationExceptions(
-            MethodArgumentNotValidException ex) {
+        // Tratamento global de exceções, impede que o Spring Security intercepte no envio de erros para rotas com .permitAll()
+        @ExceptionHandler(Exception.class)
+        public ResponseEntity<ErrorsDTO> handleAll(
+                Exception ex) {
 
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error ->
-                errors.put(error.getField(), error.getDefaultMessage())
-        );
+                ErrorsDTO error = new ErrorsDTO(
+                        "Erro interno: " + ex.getMessage(),
+                        HttpStatus.INTERNAL_SERVER_ERROR.value()
+                );
 
-        ErrorsDTO error = new ErrorsDTO(
-                errors,
-                HttpStatus.BAD_REQUEST.value()
-        );
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+        }
 
-        return ResponseEntity.badRequest().body(error);
-    }
+        // Tratamento de exceções do @Valid
+        @ExceptionHandler(MethodArgumentNotValidException.class)
+        public ResponseEntity<ErrorsDTO> handleValidationExceptions(
+                MethodArgumentNotValidException ex) {
 
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<ErrorsDTO> handleEntityNotFoundException(
-            EntityNotFoundException ex) {
+                Map<String, String> errors = new HashMap<>();
+                ex.getBindingResult().getFieldErrors().forEach(error ->
+                        errors.put(error.getField(), error.getDefaultMessage())
+                );
 
-        ErrorsDTO error = new ErrorsDTO(
-                ex.getMessage(),
-                HttpStatus.NOT_FOUND.value()
-        );
+                ErrorsDTO error = new ErrorsDTO(
+                        errors,
+                        HttpStatus.BAD_REQUEST.value()
+                );
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
-    }
+                return ResponseEntity.badRequest().body(error);
+        }
 
-    @ExceptionHandler(NoResourceFoundException.class)
-    public ResponseEntity<ErrorsDTO> handleNoResourceFoundException(
-            NoResourceFoundException ex) {
+        // Tratamento de exceções de entidades não encontradas
+        @ExceptionHandler(EntityNotFoundException.class)
+        public ResponseEntity<ErrorsDTO> handleEntityNotFoundException(
+                EntityNotFoundException ex) {
 
-        ErrorsDTO error = new ErrorsDTO(
-                "Rota não encontrada: " + ex.getResourcePath(),
-                HttpStatus.NOT_FOUND.value()
-        );
+                ErrorsDTO error = new ErrorsDTO(
+                        ex.getMessage(),
+                        HttpStatus.NOT_FOUND.value()
+                );
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
-    }
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+        }
+
+        // Tratamento de exceções de rotas http incorretas
+        @ExceptionHandler(NoResourceFoundException.class)
+        public ResponseEntity<ErrorsDTO> handleNoResourceFoundException(
+                NoResourceFoundException ex) {
+
+                ErrorsDTO error = new ErrorsDTO(
+                        "Rota não encontrada: " + ex.getResourcePath(),
+                        HttpStatus.NOT_FOUND.value()
+                );
+
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+        }
 
 }
