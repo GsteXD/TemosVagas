6011d47d9e83f0114fed7df4491c89993d34edee
Gustavo Tsuru <g.tsuruendo@gmail.com>
2025-06-24 20:42:57 -0300

    :recycle: refactor: Adicionando o contexto de Empresa

--- src/main/java/br/fatec/TemosVagas/controllers/VagaController.java
+++ src/main/java/br/fatec/TemosVagas/controllers/VagaController.java
@@ -13,6 +13,9 @@ import br.fatec.TemosVagas.dtos.vaga.VagaResponseDTO;
 import br.fatec.TemosVagas.entities.Vaga;
 import br.fatec.TemosVagas.services.VagaService;
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/vaga")
@@ -24,6 +27,12 @@ public class VagaController {
     @PostMapping("/cadastrar")
     public @ResponseBody ResponseEntity<VagaResponseDTO> cadastrar(@RequestBody @Valid VagaDTO dto) {
         Vaga vaga = vagaService.cadastrar(VagaDTO.toVaga(dto));
+        return ResponseEntity.ok().body(VagaResponseDTO.valueOf(vaga));
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<VagaResponseDTO> atualizar(@PathVariable Long id, @RequestBody @Valid VagaDTO dto) {
+        Vaga vaga = vagaService.atualizar(id, VagaDTO.toVaga(dto));
         return ResponseEntity.ok().body(VagaResponseDTO.valueOf(vaga));
     }
 }
