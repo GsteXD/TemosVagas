8fb9a9f08fa2498cfdb9b8706d5e42f5c48e2f8d
Gustavo Tsuru <g.tsuruendo@gmail.com>
2025-06-25 21:39:50 -0300

    :recycle: refactor: Uso de contexto de login em Formação

--- src/main/java/br/fatec/TemosVagas/controllers/candidato/FormacaoController.java
+++ src/main/java/br/fatec/TemosVagas/controllers/candidato/FormacaoController.java
@@ -9,6 +9,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/candidato/formacao")
@@ -17,22 +21,28 @@ public class FormacaoController {
     @Autowired
     private FormacaoService formacaoService;
 
-    @PostMapping("/cadastrar/{id_curriculo}")
-    public @ResponseBody ResponseEntity<FormacaoDTO> cadastrar(@RequestBody FormacaoDTO formacaoDTO, @PathVariable Long id_curriculo) {
-        Formacao formacao = formacaoService.cadastrar(FormacaoDTO.toFormacao(formacaoDTO), id_curriculo);
+    @PostMapping("/cadastrar")
+    public @ResponseBody ResponseEntity<FormacaoDTO> cadastrar(@RequestBody FormacaoDTO formacaoDTO) {
+        Formacao formacao = formacaoService.cadastrar(FormacaoDTO.toFormacao(formacaoDTO));
         return ResponseEntity.ok().body(FormacaoDTO.valueOf(formacao));
     }
 
-    @DeleteMapping("/deletar/{id_curriculo}/{id_formacao}")
-    public ResponseEntity<String> deletar(@PathVariable Long id_formacao, @PathVariable Long id_curriculo) {
-        formacaoService.deletar(id_formacao, id_curriculo); // Função tipo void, nada é retornado
+    @PutMapping("/atualizar/{id}")
+    public @ResponseBody ResponseEntity<FormacaoDTO> atualizar(@RequestBody FormacaoDTO formacaoDTO, @PathVariable Long id) {
+        Formacao formacao = formacaoService.atualizar(FormacaoDTO.toFormacao(formacaoDTO), id);
+        return ResponseEntity.ok().body(FormacaoDTO.valueOf(formacao));
+    }
+
+    @DeleteMapping("/deletar/{id_formacao}")
+    public ResponseEntity<String> deletar(@PathVariable Long id_formacao) {
+        formacaoService.deletar(id_formacao); // Função tipo void, nada é retornado
         return ResponseEntity.ok().body("Formação deletada com sucesso!");
     }
 
     // Lista todos as formações a partir de um id de currículo
-    @GetMapping("/listar/{id_curriculo}")
-    public @ResponseBody ResponseEntity<List<FormacaoDTO>> listarFormacoes(@PathVariable Long id_curriculo) {
-        List<Formacao> formacoes = formacaoService.listarFormacoes(id_curriculo);
+    @GetMapping("/listar")
+    public @ResponseBody ResponseEntity<List<FormacaoDTO>> listarFormacoes() {
+        List<Formacao> formacoes = formacaoService.listarFormacoes();
         return ResponseEntity.ok().body(FormacaoDTO.valueAll(formacoes));
     }
 
--- src/main/java/br/fatec/TemosVagas/services/candidato/FormacaoService.java
+++ src/main/java/br/fatec/TemosVagas/services/candidato/FormacaoService.java
@@ -1,14 +1,18 @@
 package br.fatec.TemosVagas.services.candidato;
 
+import br.fatec.TemosVagas.entities.candidato.Candidato;
 import br.fatec.TemosVagas.entities.candidato.Curriculo;
 import br.fatec.TemosVagas.entities.candidato.Formacao;
 import br.fatec.TemosVagas.repositories.candidato.CurriculoRepository;
 import br.fatec.TemosVagas.repositories.candidato.FormacaoRepository;
 import jakarta.persistence.EntityNotFoundException;
 
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,9 +26,10 @@ public class FormacaoService {
     CurriculoRepository curriculoRepository;
 
     @Transactional
-    public Formacao cadastrar(Formacao formacao, Long id_curriculo) {
-        if (formacao != null && id_curriculo != null) {
-            Curriculo curriculo = curriculoRepository.findById(id_curriculo).orElseThrow(() -> new EntityNotFoundException("Curriculo nao encontrado."));
+    public Formacao cadastrar(Formacao formacao) {
+        if (formacao != null) {
+            Candidato candidato = (Candidato) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            Curriculo curriculo = curriculoRepository.findByCandidatoId(candidato.getId()).orElseThrow(() -> new EntityNotFoundException("Curriculo nao encontrado."));
 
             formacao.setCurriculo(curriculo);
 
@@ -37,31 +42,67 @@ public class FormacaoService {
     }
 
     @Transactional
-    public void deletar(Long id, Long id_curriculo) {
-        if (id != null && id_curriculo != null) {
-            Formacao formacao = formacaoRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Formação nao encontrada."));
+    public void deletar(Long id) {
+        Candidato candidato = (Candidato) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Curriculo curriculo = curriculoRepository.findByCandidatoId(candidato.getId())
+            .orElseThrow(() -> new EntityNotFoundException("Currículo não encontrado."));
+        if (id != null && candidato != null) {
+            Formacao formacao = formacaoRepository.findByIdAndCurriculoId(id, curriculo.getId())
+                .orElseThrow(() -> new EntityNotFoundException("Formação não encontrada para este usuário."));
 
             formacao.setCurriculo(null);
 
-            curriculoRepository.findById(id_curriculo).ifPresent(
-                    curriculo -> curriculo.getListaFormacao().remove(formacao)
+            curriculoRepository.findByCandidatoId(candidato.getId()).ifPresent(
+                    c -> c.getListaFormacao().remove(formacao)
             );
 
             formacaoRepository.delete(formacao);
         } else {
-            throw new EntityNotFoundException("IDs não fornecidos.");
+            throw new EntityNotFoundException("ID da formação não fornecido.");
         }
         
     }
 
     @Transactional(readOnly = true)
-    public List<Formacao> listarFormacoes(Long id) {
-        if (id != null && id > 0) {
-            Curriculo curriculo = curriculoRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Currículo não encontrado."));
+    public List<Formacao> listarFormacoes() {
+        Candidato candidato = (Candidato) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (candidato != null) {
+            Curriculo curriculo = curriculoRepository.findByCandidatoId(candidato.getId()).orElseThrow(() -> new EntityNotFoundException("Currículo não encontrado."));
 
             return curriculo.getListaFormacao();
         }
         throw new EntityNotFoundException("ID não fornecido ou inválido.");
+    }
+
+    @Transactional
+    public Formacao atualizar(Formacao formacao, Long id) {
+        Candidato candidato = (Candidato) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (formacao != null) {
+            Curriculo curriculo = curriculoRepository.findByCandidatoId(candidato.getId())
+                .orElseThrow(() -> new EntityNotFoundException("Currículo não encontrado."));
+            Formacao formacaoExistente = formacaoRepository.findByIdAndCurriculoId(id, curriculo.getId())
+                .orElseThrow(() -> new EntityNotFoundException("Formação não encontrada para este usuário."));
+
+            Arrays.stream(Formacao.class.getDeclaredFields())
+                .filter(field ->
+                    !Modifier.isStatic(field.getModifiers()) &&
+                    !Modifier.isFinal(field.getModifiers()))
+                .forEach(field -> {
+                    field.setAccessible(true);
+                    try {
+                        Object value = field.get(formacao);
+                        if (value != null && !value.equals(field.get(formacaoExistente))) {
+                            field.set(formacaoExistente, value);
+                        }
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException("Erro ao acessar o campo: " + field.getName(), e);
+                    }
+                });
+
+            return formacaoRepository.save(formacaoExistente);
+        }
+        return null;
+
     }
 
 }
