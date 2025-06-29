de580549f9788f9398caf6594e9fcf1db5b0b17b
Vinicius <oliveiravinicius010@gmail.com>
2025-06-28 22:37:09 -0300

    :sparkles: feature: Implementação da feature de aplicação para uma vaga.

--- src/main/java/br/fatec/TemosVagas/repositories/candidato/CurriculoRepository.java
+++ src/main/java/br/fatec/TemosVagas/repositories/candidato/CurriculoRepository.java
@@ -3,5 +3,10 @@ package br.fatec.TemosVagas.repositories.candidato;
 import br.fatec.TemosVagas.entities.candidato.Curriculo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CurriculoRepository extends JpaRepository<Curriculo, Long> {
+    Optional<Curriculo> findByCandidato_Id(Long candidatoId);
+
+    boolean existsByCandidato_Id(Long candidatoId);
 }
